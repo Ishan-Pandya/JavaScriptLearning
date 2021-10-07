324fc1fc44973156338f90f68e30f2ffba5c6190
Ishan Pandya <ishanpandya96@gmail.com>
2021-10-07 10:38:38 -0400

    added js file for assignment and learning

---
+++ 01-Fundamentals-Part-1/assignment.js
@@ -0,0 +1,6 @@
+let country = "Canada";
+let continent = "North America";
+let population = "38.01 Million";
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,2 +1,4 @@
 let js = "amazing";
 if (js === "amazing") alert("JavaScript is fun!");
+console.log(10 + 60 + 54 - 25);
+x;

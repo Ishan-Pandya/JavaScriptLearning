c5221a10880f3ff882929a19e8ac5523e791358d
Ishan Pandya <ishanpandya96@gmail.com>
2021-10-07 12:57:38 -0400

    code added for Data Types

--- 01-Fundamentals-Part-1/assignment.js
+++ 01-Fundamentals-Part-1/assignment.js
@@ -1,6 +1,20 @@
+//this is a assignment file in which after every section the assignment is done by me.
+
+//code for values and variables
 let country = "Canada";
-let continent = "North America";
-let population = "38.01 Million";
-console.log(country);
-console.log(continent);
-console.log(population);
+// let continent = "North America";
+let population = 38010000;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+//code for Data Types
+// let isIsLand;
+// let language;
+// isIsLand = "Canada";
+// console.log(typeof isIsLand);
+// isIsLand = true;
+// console.log(typeof isIsLand);
+// console.log(typeof language);
+// console.log(typeof population);
+// console.log(typeof country);
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,25 @@
-let js = "amazing";
-if (js === "amazing") alert("JavaScript is fun!");
-console.log(10 + 60 + 54 - 25);
-x;
+// Code for values and variables
+// let js = "amazing";
+// if (js === "amazing") alert("JavaScript is fun!");
+// console.log(10 + 60 + 54 - 25);
+
+// //code for data types
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+// console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Hello");
+
+// javaScriptIsFun = "yes!";
+// console.log(typeof javaScriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// type of null will show as a object
+// console.log(typeof null);

d9704465848ddb1945355f916a05deb608d073ce
Ishan Pandya <ishanpandya96@gmail.com>
2021-10-07 14:21:19 -0400

    added code for the Let, Const, and Var

--- 01-Fundamentals-Part-1/assignment.js
+++ 01-Fundamentals-Part-1/assignment.js
@@ -1,14 +1,14 @@
 //this is a assignment file in which after every section the assignment is done by me.
 
-//code for values and variables
-let country = "Canada";
+// //code for values and variables
+// let country = "Canada";
 // let continent = "North America";
-let population = 38010000;
+// let population = 38010000;
 // console.log(country);
 // console.log(continent);
 // console.log(population);
 
-//code for Data Types
+// // code for Data Types
 // let isIsLand;
 // let language;
 // isIsLand = "Canada";
@@ -18,3 +18,11 @@ let population = 38010000;
 // console.log(typeof language);
 // console.log(typeof population);
 // console.log(typeof country);
+
+// //code for let, const, var
+let language;
+language = "English";
+const country = "Canada";
+const continent = "North America";
+const isIsLand = false;
+isIsLand = true;
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,4 @@
-// Code for values and variables
+// // Code for values and variables
 // let js = "amazing";
 // if (js === "amazing") alert("JavaScript is fun!");
 // console.log(10 + 60 + 54 - 25);
@@ -10,16 +10,21 @@
 // console.log(typeof javaScriptIsFun);
 // console.log(typeof 23);
 // console.log(typeof "Hello");
-
 // javaScriptIsFun = "yes!";
 // console.log(typeof javaScriptIsFun);
-
 // let year;
 // console.log(year);
 // console.log(typeof year);
-
 // year = 1991;
 // console.log(typeof year);
-
 // type of null will show as a object
 // console.log(typeof null);
+
+// //code for let, const, var
+let age = 30;
+age = 31;
+const birthYear = 1996;
+// will received error while reassign the const value
+birthYear = 1997;
+lastName = "Pandya";
+console.log(lastName);

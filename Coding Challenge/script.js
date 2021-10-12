920fa4fdc866654fc1639458973db9e19567ea83
Ishan Pandya <ishanpandya96@gmail.com>
2021-10-12 15:37:50 -0400

    did coding challenge second

--- Coding Challenge/script.js
+++ Coding Challenge/script.js
@@ -1,16 +1,16 @@
 // First Challenge
 
-let markWeight, markHeight, johnWeight, johnHeight, markHigherBMI;
-markWeight = 78;
-markHeight = 1.69;
-johnWeight = 92;
-johnHeight = 1.95;
-markBMI = markWeight / markHeight ** 2;
-johnBMI = johnWeight / johnHeight ** 2;
-console.log(markBMI);
-console.log(johnBMI);
-markHigherBMI = markBMI > johnBMI;
-console.log(markHigherBMI);
+// let markWeight, markHeight, johnWeight, johnHeight, markHigherBMI;
+// markWeight = 78;
+// markHeight = 1.69;
+// johnWeight = 92;
+// johnHeight = 1.95;
+// markBMI = markWeight / markHeight ** 2;
+// johnBMI = johnWeight / johnHeight ** 2;
+// console.log(markBMI);
+// console.log(johnBMI);
+// markHigherBMI = markBMI > johnBMI;
+// console.log(markHigherBMI);
 
 markWeight = 95;
 markHeight = 1.88;
@@ -18,7 +18,14 @@ johnWeight = 85;
 johnHeight = 1.76;
 markBMI = markWeight / markHeight ** 2;
 johnBMI = johnWeight / johnHeight ** 2;
-console.log(markBMI);
-console.log(johnBMI);
+// console.log(markBMI);
+// console.log(johnBMI);
 markHigherBMI = markBMI > johnBMI;
-console.log(markHigherBMI);
+// console.log(markHigherBMI);
+
+// Second Challenge
+if (markBMI > johnBMI) {
+  console.log(`Mark's BMI is higher than John's BMI`);
+} else {
+  console.log(`John's BMI is higher than Mark's BMI`);
+}

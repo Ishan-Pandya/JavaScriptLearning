d44414bce53ad1a21678ff759c382cf451103382
Ishan Pandya <ishanpandya96@gmail.com>
2021-10-12 14:31:03 -0400

    did the first chellange

---
+++ Coding Challenge/script.js
@@ -0,0 +1,24 @@
+// First Challenge
+
+let markWeight, markHeight, johnWeight, johnHeight, markHigherBMI;
+markWeight = 78;
+markHeight = 1.69;
+johnWeight = 92;
+johnHeight = 1.95;
+markBMI = markWeight / markHeight ** 2;
+johnBMI = johnWeight / johnHeight ** 2;
+console.log(markBMI);
+console.log(johnBMI);
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
+markWeight = 95;
+markHeight = 1.88;
+johnWeight = 85;
+johnHeight = 1.76;
+markBMI = markWeight / markHeight ** 2;
+johnBMI = johnWeight / johnHeight ** 2;
+console.log(markBMI);
+console.log(johnBMI);
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);

8b3428219788d770754e1b94c95eed9830f5e806
Ishan Pandya <ishanpandya96@gmail.com>
2021-10-14 11:49:17 -0400

    code for function declaration and expression with assignment solution

--- 02-Fundamentals-Part-2/assignment.js
+++ 02-Fundamentals-Part-2/assignment.js
@@ -1,14 +1,35 @@
 "use strict";
+//  // Code for Function
+// function describeCountry(country, population, capitalCity) {
+//   const string = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+//   return string;
+// }
 
-function describeCountry(country, population, capitalCity) {
-  const string = `${country} has ${population} million people and its capital city is ${capitalCity}`;
-  return string;
+// const canada = describeCountry("Canada", 38, "ottawa");
+// const india = describeCountry("India", 138, "ottawa");
+// const germany = describeCountry("Germany", 83.24, "Berlin");
+
+// console.log(canada);
+// console.log(india);
+// console.log(germany);
+
+//  //  ***********Code for functions declaration***********
+// function declaration
+function percentageOfWorld1(population) {
+  return population / 79;
 }
+const India1 = percentageOfWorld1(1380);
+const China1 = percentageOfWorld1(1440);
+const UnitedS1 = percentageOfWorld1(329.5);
 
-const canada = describeCountry("Canada", 38, "ottawa");
-const india = describeCountry("India", 138, "ottawa");
-const germany = describeCountry("Germany", 83.24, "Berlin");
+console.log(India1, China1, UnitedS1);
 
-console.log(canada);
-console.log(india);
-console.log(germany);
+// Function Expression
+const percentageOfWorld2 = function (population) {
+  return population / 79;
+};
+const India2 = percentageOfWorld2(1380);
+const China2 = percentageOfWorld2(1440);
+const UnitedS2 = percentageOfWorld2(329.5);
+
+console.log(India2, China2, UnitedS2);
--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,23 +1,38 @@
 "use strict";
 
-//  //  Code for functions
-function logger() {
-  console.log("My name is Ishan Pandya");
+//  //  ***********Code for functions***********
+// function logger() {
+//   console.log("My name is Ishan Pandya");
+// }
+// // calling / running / invoking the function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+//  //  ***********Code for functions declaration***********
+// Function Declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
 }
+const age1 = calcAge1(1991);
+console.log(calcAge1);
 
-// calling / running / invoking the function
-logger();
-logger();
-logger();
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
 
-function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
-  const juice = `juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
-
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+console.log(age1, age2);
